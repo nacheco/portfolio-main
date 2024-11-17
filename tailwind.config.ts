597cb46a6a1d09a1297c1c592f033e10039e7a54
nacheco <natepacheco716@gmail.com>
2024-11-16 18:06:05 -0600

    Initial commit, navbar made

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			white: 'var(--white)',
+  			lightPurple: 'var(--lightPurple)',
+  			purple: 'var(--purple)', 
+  			deepPurple: 'var(--deepPurple)',
+  			dark: 'var(--dark)',
+  			lightDark: 'var(--lightDark)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
