566be68e04845630bd407b526387a4f54b9f569f
nacheco <natepacheco716@gmail.com>
2024-11-19 02:22:41 -0600

    Made adjustments to be able to build

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = ({ }) => {
         <div className="flex flex-col items-center justify-center h-[40rem] md:h-[50rem]">
             <h1 className="text-[150px] md:text-[200px] lg:text-[240px] tracking-tighter font-bold text-white -ml-6 -mb-6 lg:-ml-12 lg:-mb-12">Hello</h1>
             <p className="text-[17px] md:text-[22px] lg:text-[28px] text-indigo-300/70">
-                I'm Natanael Pacheco - <span className="text-transparent bg-clip-text bg-main-gradient font-semibold">UI/UX Engineer</span>
+                I&apos;m Natanael Pacheco - <span className="text-transparent bg-clip-text bg-main-gradient font-semibold">UI/UX Engineer</span>
                 <br />
                 Some interesting message should go here...
             </p>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 const Navbar = () => {
     return (
@@ -6,10 +7,10 @@ const Navbar = () => {
             <nav>
                 <div className="flex flex-wrap justify-between items-center md:mx-8 p-6 ">
                     <a href="#" className="flex items-center space-x-2 rtl:space-x-reverse">
-                        <img src="/logo/logo.svg" className="h-8" alt="Flowbite Logo" />
+                        <Image src="/logo/logo.svg" width={32} height={32} className="h-8" alt="Logo" />
                     </a>
                     <div className="flex items-center space-x-2 md:space-x-6 rtl:space-x-reverse">
-                        <a href="#">
+                        <a href="/about">
                             <Button variant="link" className="text-lightPurple text-[20px] font- hover:text-purple transition-all">About</Button>
                         </a>
                         <a href="#">
