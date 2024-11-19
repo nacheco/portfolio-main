bbfd9dc960792caa191404ab5a8557aca882254c
nacheco <natepacheco716@gmail.com>
2024-11-19 02:29:05 -0600

    Adjusted the navbar links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,14 +6,14 @@ const Navbar = () => {
         <>
             <nav>
                 <div className="flex flex-wrap justify-between items-center md:mx-8 p-6 ">
-                    <a href="#" className="flex items-center space-x-2 rtl:space-x-reverse">
+                    <a href="/" className="flex items-center space-x-2 rtl:space-x-reverse">
                         <Image src="/logo/logo.svg" width={32} height={32} className="h-8" alt="Logo" />
                     </a>
                     <div className="flex items-center space-x-2 md:space-x-6 rtl:space-x-reverse">
                         <a href="/about">
                             <Button variant="link" className="text-lightPurple text-[20px] font- hover:text-purple transition-all">About</Button>
                         </a>
-                        <a href="#">
+                        <a href="/projects">
                             <Button variant="link" className="text-lightPurple text-[20px] font-medium hover:text-purple transition-all">Projects</Button>
                         </a>
                     </div>
