9f1939ac2c043688b0857f79dbb95ed84c3bb7c2
nacheco <natepacheco716@gmail.com>
2024-11-19 02:46:52 -0600

    Swapped anchor tags with Links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,22 @@
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
     return (
         <>
             <nav>
                 <div className="flex flex-wrap justify-between items-center md:mx-8 p-6 ">
-                    <a href="/" className="flex items-center space-x-2 rtl:space-x-reverse">
+                    <Link href="/" className="flex items-center space-x-2 rtl:space-x-reverse">
                         <Image src="/logo/logo.svg" width={32} height={32} className="h-8" alt="Logo" />
-                    </a>
+                    </Link>
                     <div className="flex items-center space-x-2 md:space-x-6 rtl:space-x-reverse">
-                        <a href="/about">
+                        <Link href="/about">
                             <Button variant="link" className="text-lightPurple text-[20px] font- hover:text-purple transition-all">About</Button>
-                        </a>
-                        <a href="/projects">
+                        </Link>
+                        <Link href="/projects">
                             <Button variant="link" className="text-lightPurple text-[20px] font-medium hover:text-purple transition-all">Projects</Button>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </nav>
